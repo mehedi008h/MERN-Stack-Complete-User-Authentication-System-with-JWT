51341b6bf56e0d01658bcdd1c1b36c0431292be6
Mehedi Hasan <mehedi08h@gmail.com>
2022-03-14 20:57:58 +0600

    get currently logged in user using JWT

--- routes/auth.js
+++ routes/auth.js
@@ -3,11 +3,17 @@ const {
   registerUser,
   loginUser,
   forgotPassword,
+  resetPassword,
+  getUserProfile,
 } = require("../controller/authController");
+const { isAuthenticatedUser } = require("../middleware/auth");
 const router = express.Router();
 
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
 router.route("/password/forgot").post(forgotPassword);
+router.route("/password/reset/:token").put(resetPassword);
+
+router.route("/me").get(isAuthenticatedUser, getUserProfile);
 
 module.exports = router;
