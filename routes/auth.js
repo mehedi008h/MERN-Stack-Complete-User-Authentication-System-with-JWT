995065ee43cab53d58ae1008b8f13d6e038875e4
Mehedi Hasan <mehedi08h@gmail.com>
2022-03-14 21:13:28 +0600

    implement change password

--- routes/auth.js
+++ routes/auth.js
@@ -5,6 +5,7 @@ const {
   forgotPassword,
   resetPassword,
   getUserProfile,
+  updatePassword,
 } = require("../controller/authController");
 const { isAuthenticatedUser } = require("../middleware/auth");
 const router = express.Router();
@@ -15,5 +16,6 @@ router.route("/password/forgot").post(forgotPassword);
 router.route("/password/reset/:token").put(resetPassword);
 
 router.route("/me").get(isAuthenticatedUser, getUserProfile);
+router.route("/password/update").put(isAuthenticatedUser, updatePassword);
 
 module.exports = router;
