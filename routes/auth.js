15bcaea70e37e50daeefe5db49dfb192033c09b4
Mehedi Hasan <mehedi08h@gmail.com>
2022-03-14 20:48:16 +0600

    implement forgot password

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,13 @@
 const express = require("express");
-const { registerUser, loginUser } = require("../controller/authController");
+const {
+  registerUser,
+  loginUser,
+  forgotPassword,
+} = require("../controller/authController");
 const router = express.Router();
 
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
+router.route("/password/forgot").post(forgotPassword);
 
 module.exports = router;
