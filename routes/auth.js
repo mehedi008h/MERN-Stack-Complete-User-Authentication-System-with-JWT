a57a3396ddf03d8683fa3463444239f328a4457c
Mehedi Hasan <mehedi08h@gmail.com>
2022-03-14 21:22:13 +0600

    logout user

--- routes/auth.js
+++ routes/auth.js
@@ -6,6 +6,8 @@ const {
   resetPassword,
   getUserProfile,
   updatePassword,
+  updateProfile,
+  logout,
 } = require("../controller/authController");
 const { isAuthenticatedUser } = require("../middleware/auth");
 const router = express.Router();
@@ -14,8 +16,10 @@ router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
 router.route("/password/forgot").post(forgotPassword);
 router.route("/password/reset/:token").put(resetPassword);
+router.route("/logout").get(logout);
 
 router.route("/me").get(isAuthenticatedUser, getUserProfile);
 router.route("/password/update").put(isAuthenticatedUser, updatePassword);
+router.route("/me/update").put(isAuthenticatedUser, updateProfile);
 
 module.exports = router;
