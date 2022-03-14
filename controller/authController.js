6e9a3a45eaaf42aad402713d9f5d85422ed36064
Mehedi Hasan <mehedi08h@gmail.com>
2022-03-14 20:35:17 +0600

    login user

--- controller/authController.js
+++ controller/authController.js
@@ -2,6 +2,7 @@ const User = require("../models/user");
 
 const ErrorHandler = require("../utils/errorHandler");
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+const sendToken = require("../utils/jwtToken");
 
 // Register a user   => /api/v1/register
 exports.registerUser = catchAsyncErrors(async (req, res, next) => {
@@ -17,8 +18,31 @@ exports.registerUser = catchAsyncErrors(async (req, res, next) => {
     },
   });
 
-  res.status(200).json({
-    success: true,
-    user,
-  });
+  sendToken(user, 200, res);
+});
+
+// Login User  =>  /api/v1/login
+exports.loginUser = catchAsyncErrors(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // Checks if email and password is entered by user
+  if (!email || !password) {
+    return next(new ErrorHandler("Please enter email & password", 400));
+  }
+
+  // Finding user in database
+  const user = await User.findOne({ email }).select("+password");
+
+  if (!user) {
+    return next(new ErrorHandler("Invalid Email or Password", 401));
+  }
+
+  // Checks if password is correct or not
+  const isPasswordMatched = await user.comparePassword(password);
+
+  if (!isPasswordMatched) {
+    return next(new ErrorHandler("Invalid Email or Password", 401));
+  }
+
+  sendToken(user, 200, res);
 });
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const { registerUser } = require("../controller/authController");
+const { registerUser, loginUser } = require("../controller/authController");
 const router = express.Router();
 
 router.route("/register").post(registerUser);
+router.route("/login").post(loginUser);
 
 module.exports = router;
