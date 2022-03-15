d58ab621add034b732dc69f1ecb7cc1f35e7f080
Mehedi Hasan <mehedi08h@gmail.com>
2022-03-15 09:23:20 +0600

    implement redux

--- client/src/index.js
+++ client/src/index.js
@@ -4,11 +4,24 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Provider } from "react-redux";
+import store from "./store";
+
+import { positions, transitions, Provider as AlertProvider } from "react-alert";
+import AlertTemplate from "react-alert-template-basic";
+
+const options = {
+  timeout: 5000,
+  position: positions.TOP_RIGHT,
+  transition: transitions.SCALE,
+};
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <AlertProvider template={AlertTemplate} {...options}>
+      <App />
+    </AlertProvider>
+  </Provider>,
   document.getElementById("root")
 );
 
--- client/src/pages/auth/Login.js
+++ client/src/pages/auth/Login.js
@@ -1,23 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
+// import { useAlert } from "react-alert";
+
 import "./auth.css";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  // const alert = useAlert();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(email, password);
+  };
   return (
     <div className="login">
       <div className="login_container">
         <h3 className="text-center">Login</h3>
 
-        <div className="from_group">
-          <label>Email</label>
-          <input className="from_input" type="text" />
-        </div>
-        <div className="from_group">
-          <label>Password</label>
-          <input className="from_input" type="password" />
-        </div>
-        <div className="from_group">
-          <button>Login</button>
-        </div>
+        <form onSubmit={submitHandler}>
+          <div className="from_group">
+            <label htmlFor="email_field">Email</label>
+            <input
+              className="from_input"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              type="email"
+            />
+          </div>
+          <div className="from_group">
+            <label htmlFor="password_field">Password</label>
+            <input
+              className="from_input"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              type="password"
+            />
+          </div>
+          <div className="from_group">
+            <button>Login</button>
+          </div>
+        </form>
       </div>
     </div>
   );
