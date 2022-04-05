621f1ff170061188c56b9272a7a17fdb73ae63c6
Mehedi Hasan <mehedi08h@gmail.com>
2022-04-05 11:39:10 +0600

    fix email issue

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,23 +1,23 @@
 const nodemailer = require("nodemailer");
 
 const sendEmail = async (options) => {
-  const transporter = nodemailer.createTransport({
-    host: process.env.SMTP_HOST,
-    port: process.env.SMTP_PORT,
-    auth: {
-      user: process.env.SMTP_EMAIL,
-      pass: process.env.SMTP_PASSWORD,
-    },
-  });
+    const transporter = nodemailer.createTransport({
+        host: process.env.SMTP_HOST,
+        port: process.env.SMTP_PORT,
+        auth: {
+            user: process.env.SMTP_EMAIL,
+            pass: process.env.SMTP_PASSWORD,
+        },
+    });
 
-  const message = {
-    from: `${process.env.SMTP_FROM_NAME} <${process.env.SMTP_FROM_EMAIL}>`,
-    to: options.email,
-    subject: options.subject,
-    text: options.message,
-  };
+    const message = {
+        from: `${process.env.SMTP_FROM_NAME} <${process.env.SMTP_FROM_EMAIL}>`,
+        to: options.email,
+        subject: options.subject,
+        html: options.message,
+    };
 
-  await transporter.sendMail(message);
+    await transporter.sendMail(message);
 };
 
 module.exports = sendEmail;
