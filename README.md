310ca7fc148c9a18dcdca8ff26896e0ab384bcfe
Mehedi Hasan <mehedi08h@gmail.com>
2022-04-05 14:04:27 +0600

    design home page

---
+++ README.md
@@ -0,0 +1,18 @@
+# MERN Stack - Authentication App
+
+![NFT Marketplace](https://i.ibb.co/X5kYdvB/image.png)
+
+## Introduction
+
+This is a code repository for a MERN AUTH application. A perfected user authentication application using the MERN Stack + Redux.
+
+Include:
+
+-   Register, login with validation form.
+-   Forgot password, reset password and register a new account by Email verification.
+-   Update personal information (name, password and avatar)
+-   Change permissions and delete user accounts with Admin.
+
+-   Below is the Demo link. Thanks and have a good day!💓💓
+
+## Demo: https://mernauth08h.herokuapp.com/
--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,18 +1,45 @@
 import React from "react";
 import "./Home.css";
-import { SiSpringsecurity } from "react-icons/si";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  return (
-    <div className="home">
-      <div className="text-center ">
-        <div className="mx-auto" style={{ width: "80%" }}>
-          <SiSpringsecurity size={150} />
-          <h1 className="mt-5">Welcome to MERN Auth Application</h1>
+    const { user } = useSelector((state) => state.auth);
+    return (
+        <div className="home">
+            <div className="container">
+                <div className="row g-5">
+                    <div className="col-md-6">
+                        <div className="banner_info">
+                            <h1>Welcome to MERN Auth Application</h1>
+                            <p>
+                                A perfected user authentication application
+                                using the MERN Stack + Redux.Hit the button and
+                                enjoy it.
+                            </p>
+                            {user ? (
+                                <>
+                                    <Link to="/me">Dashboard</Link>
+                                </>
+                            ) : (
+                                <>
+                                    <Link to="/register">Register</Link>
+                                </>
+                            )}
+                        </div>
+                    </div>
+                    <div className="col-md-6">
+                        <div className="banner_img">
+                            <img
+                                src="https://res.cloudinary.com/mehedi08h/image/upload/v1649142300/Website_Creator-amico_fdiztz.svg"
+                                alt=""
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Home;
